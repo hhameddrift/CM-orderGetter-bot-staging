1e5d20129d29bc809a45dc464d187ad40bdb385a
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2022-07-13 11:33:30 -0700

    Update webhookController.js
    updated error handling with 200OK and status of success equals to true

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -7,7 +7,7 @@ const processWebhook = async(req, res) => {
     if (type === "button_clicked" && button_body === "Order Status") {
         
         console.log("Acquiring an Order Status! Preloading order from order service.")
-        res.status(200)
+        res.status(200 && res.success === "true")
         await mainService(contactId)
     } else {
         res.status(500).send("Server Error")
