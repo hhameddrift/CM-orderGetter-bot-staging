595fb0e4b303467fe4f479fcd6bfee7e7f44121d
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2022-07-20 10:04:04 -0700

    Update webhookController.js
    swapping from contactId to convoId

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -4,12 +4,14 @@ const processWebhook = async(req, res) => {
     const type = req.body.type
     const button_body = req.body.data.body
     const contactId = req.body.data.author.id
+    const convoId = req.body.data['conversationId']
+
     if (type === "button_clicked" && button_body === "Order Status") {
         
         console.log("Acquiring an Order Status! Preloading order from order service.")
         
         res.status(200)
-        await mainService(contactId)
+        await mainService(convoId)
     } else {
         res.status(500).send("Server Error")
     }
