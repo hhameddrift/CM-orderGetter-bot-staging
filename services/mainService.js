d3e6bbe299e432fb4be0f9e3590f2dc27e352af1
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2022-07-20 11:29:36 -0700

    Update mainService.js
    revamp cycle of getting attributes, place an order, save attributes to contact

--- services/mainService.js
+++ services/mainService.js
@@ -1,11 +1,19 @@
 const {orderGetter} = require("./getOrder")
 const {updateContact} = require("./updateContact");
+const {getAttributes} = require("./getContact");
+const {orderGetter} = require("./getContact");
 
-const mainService = async(contactId) => {
+
+
+const mainService = async(convoId, authorId) => {
     try {
+        
+        const contactAttributes = await getAttributes(authorId)
         const order = await orderGetter()
-        const updatedContact = await updateContact(contactId, order)
+        const updatedContact = await updateContact(authorId)
+
         console.log("Contact updated with a order status!")
+
     } catch (err) {
         console.log("order processing error: ");
         console.log(err);
