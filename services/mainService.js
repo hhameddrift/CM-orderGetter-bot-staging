53d60deabdd2b4990ae59ee99d69b248e291ca6b
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2022-08-11 10:36:06 -0700

    Update mainService.js
    add clearOrderNumber function to main processor

--- services/mainService.js
+++ services/mainService.js
@@ -1,6 +1,7 @@
 const {orderGetter} = require("./getOrder")
 const {updateContact} = require("./updateContact");
 const {getAttributes} = require("./getContact");
+const {clearOrderNumber} = require('./clearDriftAttribute');
 
 
 
@@ -10,8 +11,10 @@ const mainService = async(convoId, authorId) => {
         const contactAttributes = await getAttributes(authorId)
         const orderStatus = await orderGetter()
         const updatedContact = await updateContact(authorId)
+                console.log("Contact updated with a order status!")
+        const clearOrderNumber =  await clearOrderNumber(authorId);
+                console.log('Cleared the OrderNumber.');
 
-        console.log("Contact updated with a order status!")
 
     } catch (err) {
         console.log("order processing error: ");
