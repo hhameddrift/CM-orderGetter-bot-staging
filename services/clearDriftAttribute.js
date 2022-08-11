ba92d33b68a16ee81e81f7b3024831047dedf64f
hhameddrift <83829866+hhameddrift@users.noreply.github.com>
2022-08-11 10:29:41 -0700

    Create clearDriftAttribute.js
    add clearOrderNumber to wipe out previous orders

---
+++ services/clearDriftAttribute.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const drift_token = process.env.DRIFT_TOKEN;
+const axios = require('axios');
+const baseUrl = 'https://driftapi.com/contacts/'
+const headers = {
+    'Authorization': `Bearer ${drift_token}`,
+    'Content-Type': 'application/json'
+}
+
+const clearOrderNumber = async(id) => {
+
+    return axios
+        .patch(baseUrl + id,
+            {
+                "attributes": {
+                    "order_number": " ",
+                }
+            },
+            {
+                headers
+            }
+        ).catch(err => {
+
+            console.log(err)});
+}
+
+
+module.exports = {
+    clearOrderNumber
+}
